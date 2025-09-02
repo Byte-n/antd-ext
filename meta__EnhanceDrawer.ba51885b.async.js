35c8ca3d487088881f293ba51d18f25dccd56112
Byte-n <Byte-n@users.noreply.github.com>
2025-09-02 10:07:01 +0000

    deploy: c26235a2c4c0fe01b457026b96339ecd283ef2bc

---
+++ meta__EnhanceDrawer.ba51885b.async.js
@@ -0,0 +1,244 @@
+"use strict";(self.webpackChunk_byte_n_antd_ext=self.webpackChunk_byte_n_antd_ext||[]).push([[4386],{30183:function(s,a,n){var o;n.r(a),n.d(a,{demos:function(){return y}});var f=n(90228),d=n.n(f),x=n(87999),c=n.n(x),t=n(75271),m=n(69457),I=n(70920),h=n(25301),y={"enhancedrawer-demo-basic":{component:t.memo(t.lazy(function(){return n.e(7630).then(n.bind(n,71033))})),asset:{type:"BLOCK",id:"enhancedrawer-demo-basic",refAtomIds:["EnhanceDrawer"],dependencies:{"index.tsx":{type:"FILE",value:n(31879).Z},antd:{type:"NPM",value:"5.26.4"},"@byte.n/antd-ext":{type:"NPM",value:"1.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u57FA\u7840\u62D6\u62FD\u529F\u80FD"},context:{antd:m,"@byte.n/antd-ext":I,react:o||(o=n.t(t,2))},renderOpts:{compile:function(){var l=c()(d()().mark(function u(){var r,i=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(5413).then(n.bind(n,85413));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,i));case 3:case"end":return e.stop()}},u)}));function p(){return l.apply(this,arguments)}return p}()}},"enhancedrawer-demo-with-limits":{component:t.memo(t.lazy(function(){return n.e(7630).then(n.bind(n,57056))})),asset:{type:"BLOCK",id:"enhancedrawer-demo-with-limits",refAtomIds:["EnhanceDrawer"],dependencies:{"index.tsx":{type:"FILE",value:n(86615).Z},antd:{type:"NPM",value:"5.26.4"},"@byte.n/antd-ext":{type:"NPM",value:"1.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u62D6\u62FD\u5C3A\u5BF8\u9650\u5236"},context:{antd:m,"@byte.n/antd-ext":I,react:o||(o=n.t(t,2))},renderOpts:{compile:function(){var l=c()(d()().mark(function u(){var r,i=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(5413).then(n.bind(n,85413));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,i));case 3:case"end":return e.stop()}},u)}));function p(){return l.apply(this,arguments)}return p}()}},"enhancedrawer-demo-custom-icon":{component:t.memo(t.lazy(function(){return n.e(7630).then(n.bind(n,82482))})),asset:{type:"BLOCK",id:"enhancedrawer-demo-custom-icon",refAtomIds:["EnhanceDrawer"],dependencies:{"index.tsx":{type:"FILE",value:n(81974).Z},antd:{type:"NPM",value:"5.26.4"},"@byte.n/antd-ext":{type:"NPM",value:"1.0.1"},"@ant-design/icons":{type:"NPM",value:"5.6.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u81EA\u5B9A\u4E49\u62D6\u62FD\u56FE\u6807"},context:{antd:m,"@byte.n/antd-ext":I,"@ant-design/icons":h,react:o||(o=n.t(t,2))},renderOpts:{compile:function(){var l=c()(d()().mark(function u(){var r,i=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(5413).then(n.bind(n,85413));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,i));case 3:case"end":return e.stop()}},u)}));function p(){return l.apply(this,arguments)}return p}()}},"enhancedrawer-demo-with-events":{component:t.memo(t.lazy(function(){return n.e(7630).then(n.bind(n,39791))})),asset:{type:"BLOCK",id:"enhancedrawer-demo-with-events",refAtomIds:["EnhanceDrawer"],dependencies:{"index.tsx":{type:"FILE",value:n(18103).Z},antd:{type:"NPM",value:"5.26.4"},"@byte.n/antd-ext":{type:"NPM",value:"1.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u62D6\u62FD\u4E8B\u4EF6\u56DE\u8C03"},context:{antd:m,"@byte.n/antd-ext":I,react:o||(o=n.t(t,2))},renderOpts:{compile:function(){var l=c()(d()().mark(function u(){var r,i=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(5413).then(n.bind(n,85413));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,i));case 3:case"end":return e.stop()}},u)}));function p(){return l.apply(this,arguments)}return p}()}},"enhancedrawer-demo-different-placements":{component:t.memo(t.lazy(function(){return n.e(7630).then(n.bind(n,20918))})),asset:{type:"BLOCK",id:"enhancedrawer-demo-different-placements",refAtomIds:["EnhanceDrawer"],dependencies:{"index.tsx":{type:"FILE",value:n(18412).Z},antd:{type:"NPM",value:"5.26.4"},"@byte.n/antd-ext":{type:"NPM",value:"1.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u4E0D\u540C\u4F4D\u7F6E\u62D6\u62FD"},context:{antd:m,"@byte.n/antd-ext":I,react:o||(o=n.t(t,2))},renderOpts:{compile:function(){var l=c()(d()().mark(function u(){var r,i=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(5413).then(n.bind(n,85413));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,i));case 3:case"end":return e.stop()}},u)}));function p(){return l.apply(this,arguments)}return p}()}},"enhancedrawer-demo-theme-customization":{component:t.memo(t.lazy(function(){return n.e(7630).then(n.bind(n,37627))})),asset:{type:"BLOCK",id:"enhancedrawer-demo-theme-customization",refAtomIds:["EnhanceDrawer"],dependencies:{"index.tsx":{type:"FILE",value:n(41320).Z},antd:{type:"NPM",value:"5.26.4"},"@byte.n/antd-ext":{type:"NPM",value:"1.0.1"},react:{type:"NPM",value:"18.3.1"}},entry:"index.tsx",title:"\u4E3B\u9898\u5B9A\u5236"},context:{antd:m,"@byte.n/antd-ext":I,react:o||(o=n.t(t,2))},renderOpts:{compile:function(){var l=c()(d()().mark(function u(){var r,i=arguments;return d()().wrap(function(e){for(;;)switch(e.prev=e.next){case 0:return e.next=2,n.e(5413).then(n.bind(n,85413));case 2:return e.abrupt("return",(r=e.sent).default.apply(r,i));case 3:case"end":return e.stop()}},u)}));function p(){return l.apply(this,arguments)}return p}()}}}},57319:function(s,a,n){n.r(a),n.d(a,{texts:function(){return o}});const o=[{value:"\u589E\u5F3A\u7684 Drawer \u7EC4\u4EF6\uFF0C\u652F\u6301\u62D6\u62FD\u8C03\u6574\u5927\u5C0F\u529F\u80FD\u3002",paraId:0,tocIndex:0},{value:"\u62D6\u62FD\u8C03\u6574\u5927\u5C0F",paraId:1,tocIndex:1},{value:"\uFF1A\u652F\u6301\u901A\u8FC7\u62D6\u62FD\u624B\u67C4\u8C03\u6574\u62BD\u5C49\u7684\u5BBD\u5EA6\u6216\u9AD8\u5EA6",paraId:1,tocIndex:1},{value:"\u81EA\u5B9A\u4E49\u62D6\u62FD\u56FE\u6807",paraId:1,tocIndex:1},{value:"\uFF1A\u652F\u6301\u81EA\u5B9A\u4E49\u62D6\u62FD\u6307\u793A\u5668\u7684\u56FE\u6807",paraId:1,tocIndex:1},{value:"\u62D6\u62FD\u9650\u5236",paraId:1,tocIndex:1},{value:"\uFF1A\u652F\u6301\u8BBE\u7F6E\u62D6\u62FD\u7684\u6700\u5C0F\u548C\u6700\u5927\u5C3A\u5BF8\u9650\u5236",paraId:1,tocIndex:1},{value:"\u62D6\u62FD\u4E8B\u4EF6\u56DE\u8C03",paraId:1,tocIndex:1},{value:"\uFF1A\u652F\u6301\u62D6\u62FD\u5F00\u59CB\u548C\u7ED3\u675F\u7684\u4E8B\u4EF6\u56DE\u8C03",paraId:1,tocIndex:1},{value:"\u4E3B\u9898\u5B9A\u5236",paraId:1,tocIndex:1},{value:"\uFF1A\u652F\u6301\u901A\u8FC7 ConfigProvider \u5B9A\u5236\u62D6\u62FD\u6307\u793A\u5668\u7684\u6837\u5F0F",paraId:1,tocIndex:1},{value:"\u53C2\u6570",paraId:2,tocIndex:15},{value:"\u8BF4\u660E",paraId:2,tocIndex:15},{value:"\u7C7B\u578B",paraId:2,tocIndex:15},{value:"\u9ED8\u8BA4\u503C",paraId:2,tocIndex:15},{value:"\u7248\u672C",paraId:2,tocIndex:15},{value:"resize",paraId:2,tocIndex:15},{value:"\u662F\u5426\u542F\u7528\u62D6\u62FD\u8C03\u6574\u5927\u5C0F\u529F\u80FD",paraId:2,tocIndex:15},{value:"boolean | ResizeConfig",paraId:2,tocIndex:15},{value:"false",paraId:2,tocIndex:15},{value:"-",paraId:2,tocIndex:15},{value:"\u53C2\u6570",paraId:3,tocIndex:16},{value:"\u8BF4\u660E",paraId:3,tocIndex:16},{value:"\u7C7B\u578B",paraId:3,tocIndex:16},{value:"\u9ED8\u8BA4\u503C",paraId:3,tocIndex:16},{value:"\u7248\u672C",paraId:3,tocIndex:16},{value:"min",paraId:3,tocIndex:16},{value:"\u62D6\u62FD\u7684\u6700\u5C0F\u5C3A\u5BF8\uFF08\u50CF\u7D20\uFF09",paraId:3,tocIndex:16},{value:"number",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"max",paraId:3,tocIndex:16},{value:"\u62D6\u62FD\u7684\u6700\u5927\u5C3A\u5BF8\uFF08\u50CF\u7D20\uFF09",paraId:3,tocIndex:16},{value:"number",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"onReSizeStart",paraId:3,tocIndex:16},{value:"\u62D6\u62FD\u5F00\u59CB\u65F6\u7684\u56DE\u8C03\u51FD\u6570",paraId:3,tocIndex:16},{value:"() => void",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"onReSizeEnd",paraId:3,tocIndex:16},{value:"\u62D6\u62FD\u7ED3\u675F\u65F6\u7684\u56DE\u8C03\u51FD\u6570",paraId:3,tocIndex:16},{value:"() => void",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"dragIcon",paraId:3,tocIndex:16},{value:"\u81EA\u5B9A\u4E49\u62D6\u62FD\u56FE\u6807",paraId:3,tocIndex:16},{value:"React.ReactNode",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"-",paraId:3,tocIndex:16},{value:"\u901A\u8FC7 ConfigProvider \u7684 ",paraId:4,tocIndex:17},{value:"theme.components.EnhanceDrawer",paraId:4,tocIndex:17},{value:" \u53EF\u4EE5\u5B9A\u5236\u62D6\u62FD\u6307\u793A\u5668\u7684\u6837\u5F0F\u3002",paraId:4,tocIndex:17},{value:"\u53C2\u6570",paraId:5,tocIndex:18},{value:"\u8BF4\u660E",paraId:5,tocIndex:18},{value:"\u7C7B\u578B",paraId:5,tocIndex:18},{value:"\u9ED8\u8BA4\u503C",paraId:5,tocIndex:18},{value:"\u7248\u672C",paraId:5,tocIndex:18},{value:"indicatorDraggingColor",paraId:5,tocIndex:18},{value:"\u62D6\u62FD\u6307\u793A\u5668\u5728\u62D6\u62FD\u72B6\u6001\u4E0B\u7684\u80CC\u666F\u8272",paraId:5,tocIndex:18},{value:"string",paraId:5,tocIndex:18},{value:"token.colorPrimaryBg",paraId:5,tocIndex:18},{value:"-",paraId:5,tocIndex:18},{value:"indicatorIconColor",paraId:5,tocIndex:18},{value:"\u62D6\u62FD\u56FE\u6807\u7684\u989C\u8272",paraId:5,tocIndex:18},{value:"string",paraId:5,tocIndex:18},{value:"token.colorTextSecondary",paraId:5,tocIndex:18},{value:"-",paraId:5,tocIndex:18},{value:"indicatorIconSize",paraId:5,tocIndex:18},{value:"\u62D6\u62FD\u56FE\u6807\u7684\u5927\u5C0F\uFF08\u50CF\u7D20\uFF09",paraId:5,tocIndex:18},{value:"number",paraId:5,tocIndex:18},{value:"16",paraId:5,tocIndex:18},{value:"-",paraId:5,tocIndex:18},{value:"indicatorIconBgColor",paraId:5,tocIndex:18},{value:"\u62D6\u62FD\u56FE\u6807\u7684\u80CC\u666F\u8272",paraId:5,tocIndex:18},{value:"string",paraId:5,tocIndex:18},{value:"token.colorBgContainer",paraId:5,tocIndex:18},{value:"-",paraId:5,tocIndex:18}]},31879:function(s,a){a.Z=`import { Button, ConfigProvider } from 'antd';
+import { EnhanceDrawer } from '@byte.n/antd-ext';
+import React, { useState } from 'react';
+
+export default () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ConfigProvider>
+      <Button onClick={() => setOpen(true)}>\u6253\u5F00\u62BD\u5C49</Button>
+      <EnhanceDrawer
+        title="\u57FA\u7840\u62D6\u62FD\u529F\u80FD"
+        open={open}
+        onClose={() => setOpen(false)}
+        resize={true}
+      >
+        <p>\u8FD9\u662F\u4E00\u4E2A\u652F\u6301\u62D6\u62FD\u8C03\u6574\u5927\u5C0F\u7684\u62BD\u5C49\u3002</p>
+        <p>\u4F60\u53EF\u4EE5\u62D6\u62FD\u53F3\u4FA7\u8FB9\u7F18\u6765\u8C03\u6574\u62BD\u5C49\u7684\u5BBD\u5EA6\u3002</p>
+        <p>\u62D6\u62FD\u624B\u67C4\u4F1A\u663E\u793A\u5728\u62BD\u5C49\u7684\u8FB9\u7F18\uFF0C\u9F20\u6807\u60AC\u505C\u65F6\u4F1A\u663E\u793A\u62D6\u62FD\u56FE\u6807\u3002</p>
+      </EnhanceDrawer>
+    </ConfigProvider>
+  );
+};
+`},81974:function(s,a){a.Z=`import { Button, ConfigProvider } from 'antd';
+import { EnhanceDrawer } from '@byte.n/antd-ext';
+import { MenuOutlined, DragOutlined, SettingOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+
+export default () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ConfigProvider>
+      <Button onClick={() => setOpen(true)}>\u6253\u5F00\u62BD\u5C49</Button>
+      <EnhanceDrawer
+        title="\u81EA\u5B9A\u4E49\u62D6\u62FD\u56FE\u6807"
+        open={open}
+        onClose={() => setOpen(false)}
+        resize={{
+          dragIcon: <MenuOutlined style={{ fontSize: 20, color: '#1890ff' }} />,
+        }}
+      >
+        <p>\u8FD9\u4E2A\u62BD\u5C49\u4F7F\u7528\u4E86\u81EA\u5B9A\u4E49\u7684\u62D6\u62FD\u56FE\u6807\u3002</p>
+        <p>\u4F60\u53EF\u4EE5\u901A\u8FC7 <code>resize.dragIcon</code> \u5C5E\u6027\u6765\u81EA\u5B9A\u4E49\u62D6\u62FD\u56FE\u6807\u3002</p>
+        <p>\u652F\u6301\u4EFB\u4F55 React \u7EC4\u4EF6\u4F5C\u4E3A\u62D6\u62FD\u56FE\u6807\u3002</p>
+      </EnhanceDrawer>
+    </ConfigProvider>
+  );
+};
+`},18412:function(s,a){a.Z=`import { Button, ConfigProvider } from 'antd';
+import { EnhanceDrawer } from '@byte.n/antd-ext';
+import React, { useState } from 'react';
+
+export default () => {
+  const [openRight, setOpenRight] = useState(false);
+  const [openLeft, setOpenLeft] = useState(false);
+  const [openTop, setOpenTop] = useState(false);
+  const [openBottom, setOpenBottom] = useState(false);
+
+  return (
+    <ConfigProvider>
+      <div style={{ marginBottom: 16 }}>
+        <Button onClick={() => setOpenRight(true)} style={{ marginRight: 8 }}>
+          \u53F3\u4FA7\u62BD\u5C49
+        </Button>
+        <Button onClick={() => setOpenLeft(true)} style={{ marginRight: 8 }}>
+          \u5DE6\u4FA7\u62BD\u5C49
+        </Button>
+        <Button onClick={() => setOpenTop(true)} style={{ marginRight: 8 }}>
+          \u9876\u90E8\u62BD\u5C49
+        </Button>
+        <Button onClick={() => setOpenBottom(true)}>
+          \u5E95\u90E8\u62BD\u5C49
+        </Button>
+      </div>
+
+      <EnhanceDrawer
+        title="\u53F3\u4FA7\u62BD\u5C49"
+        open={openRight}
+        placement="right"
+        onClose={() => setOpenRight(false)}
+        resize={{
+          min: 300,
+          max: 800,
+        }}
+      >
+        <p>\u8FD9\u662F\u4E00\u4E2A\u53F3\u4FA7\u62BD\u5C49\u3002</p>
+        <p>\u62D6\u62FD\u53F3\u4FA7\u8FB9\u7F18\u53EF\u4EE5\u8C03\u6574\u5BBD\u5EA6\u3002</p>
+        <p>\u62D6\u62FD\u56FE\u6807\u4F1A\u663E\u793A\u4E3A\u6C34\u5E73\u65B9\u5411\u3002</p>
+      </EnhanceDrawer>
+
+      <EnhanceDrawer
+        title="\u5DE6\u4FA7\u62BD\u5C49"
+        open={openLeft}
+        placement="left"
+        onClose={() => setOpenLeft(false)}
+        resize={{
+          min: 300,
+          max: 800,
+        }}
+      >
+        <p>\u8FD9\u662F\u4E00\u4E2A\u5DE6\u4FA7\u62BD\u5C49\u3002</p>
+        <p>\u62D6\u62FD\u5DE6\u4FA7\u8FB9\u7F18\u53EF\u4EE5\u8C03\u6574\u5BBD\u5EA6\u3002</p>
+        <p>\u62D6\u62FD\u56FE\u6807\u4F1A\u663E\u793A\u4E3A\u6C34\u5E73\u65B9\u5411\u3002</p>
+      </EnhanceDrawer>
+
+      <EnhanceDrawer
+        title="\u9876\u90E8\u62BD\u5C49"
+        open={openTop}
+        placement="top"
+        onClose={() => setOpenTop(false)}
+        resize={{
+          min: 200,
+          max: 600,
+        }}
+      >
+        <p>\u8FD9\u662F\u4E00\u4E2A\u9876\u90E8\u62BD\u5C49\u3002</p>
+        <p>\u62D6\u62FD\u9876\u90E8\u8FB9\u7F18\u53EF\u4EE5\u8C03\u6574\u9AD8\u5EA6\u3002</p>
+        <p>\u62D6\u62FD\u56FE\u6807\u4F1A\u663E\u793A\u4E3A\u5782\u76F4\u65B9\u5411\u3002</p>
+      </EnhanceDrawer>
+
+      <EnhanceDrawer
+        title="\u5E95\u90E8\u62BD\u5C49"
+        open={openBottom}
+        placement="bottom"
+        onClose={() => setOpenBottom(false)}
+        resize={{
+          min: 200,
+          max: 600,
+        }}
+      >
+        <p>\u8FD9\u662F\u4E00\u4E2A\u5E95\u90E8\u62BD\u5C49\u3002</p>
+        <p>\u62D6\u62FD\u5E95\u90E8\u8FB9\u7F18\u53EF\u4EE5\u8C03\u6574\u9AD8\u5EA6\u3002</p>
+        <p>\u62D6\u62FD\u56FE\u6807\u4F1A\u663E\u793A\u4E3A\u5782\u76F4\u65B9\u5411\u3002</p>
+      </EnhanceDrawer>
+    </ConfigProvider>
+  );
+};
+`},41320:function(s,a){a.Z=`import { Button, ConfigProvider } from 'antd';
+import { EnhanceDrawer } from '@byte.n/antd-ext';
+import React, { useState } from 'react';
+
+export default () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ConfigProvider
+      theme={{
+        components: {
+          EnhanceDrawer: {
+            indicatorDraggingColor: '#f0f8ff',
+            indicatorIconColor: '#1890ff',
+            indicatorIconSize: 20,
+            indicatorIconBgColor: '#ffffff',
+          },
+        },
+      }}
+    >
+      <Button onClick={() => setOpen(true)}>\u6253\u5F00\u62BD\u5C49</Button>
+      <EnhanceDrawer
+        title="\u4E3B\u9898\u5B9A\u5236"
+        open={open}
+        onClose={() => setOpen(false)}
+        resize={true}
+      >
+        <p>\u8FD9\u4E2A\u62BD\u5C49\u4F7F\u7528\u4E86\u81EA\u5B9A\u4E49\u4E3B\u9898\uFF1A</p>
+        <ul>
+          <li>\u62D6\u62FD\u80CC\u666F\u8272\uFF1A\u6D45\u84DD\u8272 (#f0f8ff)</li>
+          <li>\u56FE\u6807\u989C\u8272\uFF1A\u84DD\u8272 (#1890ff)</li>
+          <li>\u56FE\u6807\u5927\u5C0F\uFF1A20px</li>
+          <li>\u56FE\u6807\u80CC\u666F\u8272\uFF1A\u767D\u8272 (#ffffff)</li>
+        </ul>
+        <p>\u4F60\u53EF\u4EE5\u901A\u8FC7 ConfigProvider \u7684 theme \u914D\u7F6E\u6765\u81EA\u5B9A\u4E49\u8FD9\u4E9B\u6837\u5F0F\u3002</p>
+      </EnhanceDrawer>
+    </ConfigProvider>
+  );
+};
+`},18103:function(s,a){a.Z=`import { Button, ConfigProvider, message } from 'antd';
+import { EnhanceDrawer } from '@byte.n/antd-ext';
+import React, { useState } from 'react';
+
+export default () => {
+  const [open, setOpen] = useState(false);
+
+  const handleResizeStart = () => {
+    message.info('\u5F00\u59CB\u62D6\u62FD\u8C03\u6574\u5927\u5C0F');
+  };
+
+  const handleResizeEnd = () => {
+    message.success('\u62D6\u62FD\u8C03\u6574\u5927\u5C0F\u5B8C\u6210');
+  };
+
+  return (
+    <ConfigProvider>
+      <Button onClick={() => setOpen(true)}>\u6253\u5F00\u62BD\u5C49</Button>
+      <EnhanceDrawer
+        title="\u62D6\u62FD\u4E8B\u4EF6\u56DE\u8C03"
+        open={open}
+        onClose={() => setOpen(false)}
+        resize={{
+          onReSizeStart: handleResizeStart,
+          onReSizeEnd: handleResizeEnd,
+        }}
+      >
+        <p>\u8FD9\u4E2A\u62BD\u5C49\u6709\u62D6\u62FD\u4E8B\u4EF6\u56DE\u8C03\uFF1A</p>
+        <ul>
+          <li>\u5F00\u59CB\u62D6\u62FD\u65F6\u4F1A\u663E\u793A\u63D0\u793A\u4FE1\u606F</li>
+          <li>\u7ED3\u675F\u62D6\u62FD\u65F6\u4F1A\u663E\u793A\u6210\u529F\u4FE1\u606F</li>
+        </ul>
+        <p>\u5C1D\u8BD5\u62D6\u62FD\u8C03\u6574\u5927\u5C0F\uFF0C\u89C2\u5BDF\u63A7\u5236\u53F0\u548C\u6D88\u606F\u63D0\u793A\u3002</p>
+      </EnhanceDrawer>
+    </ConfigProvider>
+  );
+};
+`},86615:function(s,a){a.Z=`import { Button, ConfigProvider } from 'antd';
+import { EnhanceDrawer } from '@byte.n/antd-ext';
+import React, { useState } from 'react';
+
+export default () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ConfigProvider>
+      <Button onClick={() => setOpen(true)}>\u6253\u5F00\u62BD\u5C49</Button>
+      <EnhanceDrawer
+        title="\u62D6\u62FD\u5C3A\u5BF8\u9650\u5236"
+        open={open}
+        onClose={() => setOpen(false)}
+        resize={{
+          min: 300,
+          max: 800,
+        }}
+      >
+        <p>\u8FD9\u4E2A\u62BD\u5C49\u7684\u62D6\u62FD\u6709\u5C3A\u5BF8\u9650\u5236\uFF1A</p>
+        <ul>
+          <li>\u6700\u5C0F\u5BBD\u5EA6\uFF1A300px</li>
+          <li>\u6700\u5927\u5BBD\u5EA6\uFF1A800px</li>
+        </ul>
+        <p>\u5C1D\u8BD5\u62D6\u62FD\u8C03\u6574\u5927\u5C0F\uFF0C\u4F60\u4F1A\u53D1\u73B0\u62BD\u5C49\u7684\u5BBD\u5EA6\u4E0D\u4F1A\u5C0F\u4E8E 300px \u6216\u5927\u4E8E 800px\u3002</p>
+      </EnhanceDrawer>
+    </ConfigProvider>
+  );
+};
+`}}]);
